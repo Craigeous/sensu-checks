a08c311ee900c5c99e959f3451b6637fed24ac6f
Craigeous <craigeous@gmail.com>
2016-08-15 17:11:23 -0500

    Update check_memory.ps1
    the PSScriptRoot variable does not work in all instances with PS lower than v3.  Added logic to fix that.  Will update the other scripts as well.

--- check_memory.ps1
+++ check_memory.ps1
@@ -1,6 +1,10 @@
 # Checks Memory and reports highest utililzation process if warn/crit
 param([int] $warn = 60, [int] $critical = 90)
 
+if (!$PSScriptRoot) {
+        $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Definition -Parent
+    }
+
 . (Join-Path $PSScriptRoot checks_helper.ps1)
 
 Perform-Counter-Check -counterPath "\Memory\% Committed Bytes In Use" -outputstring "Used memory at {0} %"  -warn $warn -critical $critical
