44eef1654ac4cb9333a589c8c024305971920e1e
Craigeous <craigeous@gmail.com>
2016-08-15 17:10:20 -0500

    Update check_disk_all.ps1
    the PSScriptRoot variable does not work in all instances with PS lower than v3.  Added logic to fix that.  Will update the other scripts as well.

--- check_disk_all.ps1
+++ check_disk_all.ps1
@@ -1,5 +1,10 @@
 param([int] $warn = 20, [int] $critical = 10)
 
+#This is to allow this to function properly in both PowerShell 2 and PowerShell 3 and beyond
+if (!$PSScriptRoot) {
+        $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Definition -Parent
+    }
+
 . (Join-Path $PSScriptRoot checks_helper.ps1)
 
 $countervaluearray = (Get-counter  -Counter "\LogicalDisk(*)\% Free Space").countersamples | where {$_.InstanceName -ne "_total" -and $_.InstanceName -notmatch "harddiskvolume"} | Sort-Object InstanceName
