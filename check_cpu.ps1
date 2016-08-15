f5ca28c85f1af8b45967fe067ff244452fda53a4
Craigeous <craigeous@gmail.com>
2016-08-15 17:08:34 -0500

    Update check_cpu.ps1
    the PSScriptRoot variable does not work in all instances with PS lower than v3.  Added logic to fix that.  Will update the other scripts as well.

--- check_cpu.ps1
+++ check_cpu.ps1
@@ -1,6 +1,11 @@
 # Checks CPU & Memory and reports highest utililzation process if warn/crit
 param([int] $warn = 80, [int] $critical = 90)
 
+#This is to allow this to function properly in both PowerShell 2 and PowerShell 3 and beyond
+if (!$PSScriptRoot) {
+        $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Definition -Parent
+    }
+
 . (Join-Path $PSScriptRoot checks_helper.ps1)
 
 
